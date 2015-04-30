a24f70e61f31b6eec52d92bd40ff75cb8f119cd1
Kilian Koeltzsch <me@kilian.io>
2015-05-01 00:18:42 +0200

    Reflect current state in sinatra server
    I really have a thing for tiny servers throwing back json, it's so
    pretty!

--- server.rb
+++ server.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require 'json'
+require_relative 'mensa'
 
-get '/' do
-  'Hello, world :)'
+get '/mensa' do
+  scraper = MensaScraper.new
+  mensa_list = []
+  scraper.mensa_list do |m|
+    mensa_list << m
+  end
+  JSON.dump(mensa_list)
+end
+
+get '/mensa/:name' do |name|
+  scraper = MensaScraper.new
+  meal_list = []
+  scraper.mensa_menu name do |m|
+    meal_list << m
+  end
+  JSON.dump(meal_list)
 end
